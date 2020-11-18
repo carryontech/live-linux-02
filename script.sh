b7d929eebe8a0d1595df0415007b08a12f437533
CarryOn Tech <55155715+carryontech@users.noreply.github.com>
2020-11-17 21:14:11 -0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install squid -y
 mkdir /etc/squid/regras
-cp ./sites_bloqueados /etc/squid/regras
-cp -n ./squid.conf /etc/squid/squid.conf
+cp /tmp/live-linux-02/sites_bloqueados.txt /etc/squid/regras
+cp -n /tmp/live-linux-02/squid.conf /etc/squid/squid.conf
 systemctl start squid
 systemctl enable squid
